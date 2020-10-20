1dd503803c839cffd80a80a148a1900966d3bf54
Simran <patel.simran.2104@gmail.com>
2020-10-20 19:28:16 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/university/studentcorner/repository/RegistrationRepository.java
+++ src/main/java/com/university/studentcorner/repository/RegistrationRepository.java
@@ -2,7 +2,9 @@ package com.university.studentcorner.repository;
 
 import com.university.studentcorner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RegistrationRepository  extends JpaRepository<User, Integer> {
 
     public User findByUserNameAndPassword(String emailId, String enrollmentNo);
